28c9a66a60ecb053050be8b313370f0eea96207f
Timothy Hardy <timothyhardyg@gmail.com>
2026-07-10 14:47:58 -0600

    bugfix

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ df = df[models + nonmodels]
 
 ## Scoring code
 
-features = df[defenses]
+features = df[models]
 p = features.mean(axis=1)
 
 df['proportion_right'] = features.eq(df['gold_label'], axis=0).mean(axis=1)
